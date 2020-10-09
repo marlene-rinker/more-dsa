84685249fbe6a2a59c3a161c4e5f357db4921d8e
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-10-09 09:21:16 -0700

    working on superbalanced tree question

--- InterviewCake/superbalanced-binary-tree/superbalanced-binary-tree.js
+++ InterviewCake/superbalanced-binary-tree/superbalanced-binary-tree.js
@@ -1,9 +1,36 @@
 'use strict';
 
-function isSuperbalanced(tree) {
+function isSuperbalanced(root) {
 //use depth-first traversal to find the leaves (nodes with no children);
 //count to get depth to leaf
 //when get to first leaf, save depth, then compare to depth of next leaf, if more than 1, return false, if not, keep going keeping minimum depth as the depth to compare for next leaf
+
+if(!root) {
+  return false;
+}
+
+let stack = [];
+stack.push(root);
+
+
+while(stack.length > 0) {
+  let node = stack.pop();
+
+  if(node.left) {
+    stack.push(node.left);
+  }
+
+  if(node.right) {
+    stack.push(node.right);
+  }
+
+  if(!node.right && !node.left) {
+    if ((depth - count) > 1) {
+      return false;
+    }
+  }
+
+}
 
 
 
