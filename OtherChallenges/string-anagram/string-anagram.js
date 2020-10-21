40ffb8f7f6ee307f1f0338534b481bee625e1d4e
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-10-21 09:50:01 -0700

    more for string anagrams question

--- OtherChallenges/string-anagram/string-anagram.js
+++ OtherChallenges/string-anagram/string-anagram.js
@@ -1,15 +1,79 @@
 'use strict';
 
+//helper functions
 
+function formatString(str) {
+  if(!str) {
+    return;
+  }
+  return str.replace(/[^a-zA-z]+/g, '').toLowerCase();
+}
 
+function alphabetizeString(str) {
+  if(!str) {
+    return;
+  }
+  return str.split('').sort().join('');
+}
+
+function characterMap(word) {
+  if(!word) {
+    return;
+  }
+  let charMap = {};
+  for(let i in word) {
+    let letter = word[i];
+
+    if(charMap[letter]) {
+      charMap[letter] =+1;
+    } else {
+      charMap[letter] = 1;
+    }
+  }
+  return charMap;
+}
+
+//using string comparison
 function areAnagrams(str1, str2) {
-  //remove non-alphabet characters, convert to lowercase, split out the letters, sort them, then join them back together
-  let first = str1.replace(/[^a-zA-z]+/g, '').toLowerCase().split('').sort().join('');
-  let second = str2.replace(/[^a-zA-z]+/g, '').toLowerCase().split('').sort().join('');
+  
+  let first = formatString(str1);
+  let second = formatString(str2);
+  first = alphabetizeString(first);
+  second = alphabetizeString(second);
   
   return first === second;
+
+};
+
+//using character map
+function anotherAreAnagrams(str1, str2) {
+
+  str1 = formatString(str1);
+  str2 = formatString(str2);
+  
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let firstMap = characterMap(str1);
+  let secondMap = characterMap(str2);
+  // console.log('firstMap: ', firstMap);
+  // console.log('secondMap: ', secondMap);
+
+  for (let char in firstMap) {
+    if (firstMap[char] !== secondMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+
 
 
 }
 
-module.exports = areAnagrams;
+module.exports = {
+  areAnagrams,
+  anotherAreAnagrams,
+}
+
+console.log(anotherAreAnagrams('post', 'tops'));
--- OtherChallenges/string-anagram/string-anagram.test.js
+++ OtherChallenges/string-anagram/string-anagram.test.js
@@ -1,27 +1,51 @@
 'use strict';
 
-const areAnagrams = require('./string-anagram.js');;
+const test = require('./string-anagram.js');;
 
 describe('Testing if strings are anagrams', () => {
   it ('should return true if strings are anagrams', () => {
 
   
-    expect(areAnagrams('cat', 'act')).toEqual(true);
-    expect(areAnagrams('tar', 'rat')).toEqual(true);
-    expect(areAnagrams('post', 'tops')).toEqual(true);
-    expect(areAnagrams('top', 'pot')).toEqual(true);
-    expect(areAnagrams('dormitory', 'dirty room')).toEqual(true);
-    expect(areAnagrams('the eyes', 'they see')).toEqual(true);
+    expect(test.areAnagrams('cat', 'act')).toEqual(true);
+    expect(test.areAnagrams('tar', 'rat')).toEqual(true);
+    expect(test.areAnagrams('post', 'tops')).toEqual(true);
+    expect(test.areAnagrams('top', 'pot')).toEqual(true);
+    expect(test.areAnagrams('dormitory', 'dirty room')).toEqual(true);
+    expect(test.areAnagrams('the eyes', 'they see')).toEqual(true);
   })
 
   it ('should return false if strings are not anagrams', () => {
 
   
-    expect(areAnagrams('cat', 'dog')).toEqual(false);
-    expect(areAnagrams('tarp', 'rat')).toEqual(false);
-    expect(areAnagrams('pot', 'tops')).toEqual(false);
-    expect(areAnagrams('gym', 'gypsy')).toEqual(false);
-    expect(areAnagrams('dormitory', 'dorm room')).toEqual(false);
-    expect(areAnagrams('the eyes', 'are blue')).toEqual(false);
+    expect(test.areAnagrams('cat', 'dog')).toEqual(false);
+    expect(test.areAnagrams('tarp', 'rat')).toEqual(false);
+    expect(test.areAnagrams('pot', 'tops')).toEqual(false);
+    expect(test.areAnagrams('gym', 'gypsy')).toEqual(false);
+    expect(test.areAnagrams('dormitory', 'dorm room')).toEqual(false);
+    expect(test.areAnagrams('the eyes', 'are blue')).toEqual(false);
   })
 });  
+
+describe('Testing if strings are anagrams using character maps', () => {
+  it ('should return true if strings are anagrams', () => {
+
+  
+    expect(test.anotherAreAnagrams('cat', 'act')).toEqual(true);
+    expect(test.anotherAreAnagrams('tar', 'rat')).toEqual(true);
+    expect(test.anotherAreAnagrams('post', 'tops')).toEqual(true);
+    expect(test.anotherAreAnagrams('top', 'pot')).toEqual(true);
+    expect(test.anotherAreAnagrams('dormitory', 'dirty room')).toEqual(true);
+    expect(test.anotherAreAnagrams('the eyes', 'they see')).toEqual(true);
+  })
+
+  it ('should return false if strings are not anagrams', () => {
+
+  
+    expect(test.anotherAreAnagrams('cat', 'dog')).toEqual(false);
+    expect(test.anotherAreAnagrams('tarp', 'rat')).toEqual(false);
+    expect(test.anotherAreAnagrams('pot', 'tops')).toEqual(false);
+    expect(test.anotherAreAnagrams('gym', 'gypsy')).toEqual(false);
+    expect(test.anotherAreAnagrams('dormitory', 'dorm room')).toEqual(false);
+    expect(test.anotherAreAnagrams('the eyes', 'are blue')).toEqual(false);
+  })
+});
