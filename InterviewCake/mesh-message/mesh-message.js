c7a49b1187a317180df008980795dc0b73433cb8
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-10-28 15:13:21 -0700

    started on mesh message question

---
+++ InterviewCake/mesh-message/mesh-message.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//function takes in two strings of usernames (sender and recipient) and an graph that represents the network(using an adjacency list - object where users are key and neighbors are the values in adjacency lists (arrays))
+
+//it returns an array of usernames to represent the route from sender to recipient
+
+function meshMessage(sender, recipient, network) {
+  let route = [];
+
+  
+
+  
+
+  
+
+  
+
+
+
+  return route;
+}
