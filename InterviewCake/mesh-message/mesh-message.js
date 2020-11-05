c968725f32cf7b406e80f7d0288f2a0d0080483f
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-11-04 16:47:52 -0800

    mesh message in progress

--- InterviewCake/mesh-message/mesh-message.js
+++ InterviewCake/mesh-message/mesh-message.js
@@ -6,16 +6,46 @@
 
 function meshMessage(sender, recipient, network) {
   let route = [];
+  let visited = new Set();
+  let queue = [];
+  // route.push(sender);
+  queue.push(sender);
+  visited.add(sender);
 
-  
+  while(queue.length > 0) {
+    let node = queue.shift();
+    route.push(node);
+    if (node === recipient) {
+      return route;
+    }
 
-  
-
-  
-
-  
-
-
-
+    for (let neighbor of network[node]) {
+      if (neighbor === recipient) {
+        route.push(neighbor);
+        return route;
+      }
+      if(!visited.has(neighbor)) {
+        // route.push(neighbor);
+        queue.push(neighbor);
+        visited.add(neighbor);
+      }
+    }
+  }
   return route;
+
 }
+
+module.exports = meshMessage;
+
+let network = {
+  'Marlene': ['Dave', 'Paul', 'Gloria'],
+  'Dave': ['Marlene', 'James', 'Gloria'],
+  'Paul': ['Marlene', 'Nora', 'Maisie'],
+  'Gloria': ['Marlene', 'Dave', 'Maisie'],
+  'Maisie': ['Paul', 'Gloria'],
+  'Nora': ['Paul'],
+  'James': ['Dave'],
+}
+
+console.log(meshMessage('James', 'Nora', network));
+  
